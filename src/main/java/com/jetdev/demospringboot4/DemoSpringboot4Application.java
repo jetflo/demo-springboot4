3c9aa85480a9d3c34cb990f973de42e09742f561
Florian Lemaire <florian@jetdev.fr>
2025-12-02 13:37:01 +0100

    :tada:

---
+++ src/main/java/com/jetdev/demospringboot4/DemoSpringboot4Application.java
@@ -0,0 +1,13 @@
+package com.jetdev.demospringboot4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringboot4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringboot4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jetdev/demospringboot4/DemoSpringboot4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetdev.demospringboot4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringboot4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
